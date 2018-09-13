6d2c41fffedf8f8ee88808fc7302b5f0a15b5297
AlexanderPampushko <pampushko@gmail.com>
2018-09-13 16:13:16 +0300

    Заготовка тестов, для функции получения списка поддерживаемых языков и направлений перевода (GetSupportedLanguages)

---
+++ src/test/java/GetListSupportedLanguagesTest.java
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+public class GetListSupportedLanguagesTest
+{
+	/**
+	 * Проверка получения полного наименования языка по коду.
+	 * <br />
+	 * Например "mi" = "Маори"
+	 */
+	public static void getFullNameOfLanguageByLanguageCode()
+	{
+	
+	}
+	
+	/**
+	 * Проверка получения доступных направлений перевода
+	 * <br />
+	 */
+	public static void getAllTranslateDirections()
+	{
+	
+	}
+	
+	/**
+	 * Проверка наличия определённого варианта перевода
+	 * <br />
+	 * Например, "uk-en"
+	 */
+	public static void checkExistingTranslateDirection()
+	{
+	
+	}
+	
+	/**
+	 * Проверка вывода списка поддерживаемых языков
+	 * <br />
+	 */
+	public static void checkListSupportedLanguages()
+	{
+	
+	}
+}
